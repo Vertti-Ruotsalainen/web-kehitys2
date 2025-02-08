c7535c0149d8ccfc9f0f1cf9422a24db59e26f58
Vertti Ruotsalainen <116634299+Vertti-Ruotsalainen@users.noreply.github.com>
2025-02-08 10:51:35 +0200

    Add files via upload

---
+++ web-kehitys2/script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.scroll-to').forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+    });
+});
